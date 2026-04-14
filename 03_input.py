9147c4f90cff60088aa4b8cf26159881e507e335
NotleDev <eltontecnico@gmail.com>
2026-04-14 14:44:05 -0300

    inclusao do 03_input

---
+++ 03_input.py
@@ -0,0 +1,9 @@
+name = input("Informe o nome \n")
+idade = int(input("Informe a idade \n"))
+altura = float(input("Informe a altura \n"))
+profissional = bool(input("É profissional?\n"))
+
+print(type(name))
+print(type(idade))
+print(type(altura)) 
+print(type(profissional)) 
