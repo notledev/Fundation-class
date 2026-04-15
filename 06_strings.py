c6b96d1e547fc453a1c4a1f4e777c8782070dc2a
NotleDev <eltontecnico@gmail.com>
2026-04-15 17:29:40 -0300

    inclusao arquivo 06

---
+++ 06_strings.py
@@ -0,0 +1,21 @@
+# case sensitive
+descricao = " elton "
+descricao1 = "Elton"
+
+print(descricao == descricao1)
+
+# texto longo em ''' '''
+Bio = ''' elton contador, 38 anos. 
+          trabalha no postalis. 
+          tem filhos e gosta de 
+          viajar.
+'''
+# multiplicacao de string
+line = "="
+print(line *50)
+print(Bio)
+print(line *50)
+
+#procurar palavras dentro de texto
+
+print("elton" in Bio)
