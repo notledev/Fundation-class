23847a1260adfe79a6e2620c5487f08c447d60b0
NotleDev <eltontecnico@gmail.com>
2026-04-15 11:26:44 -0300

    criação do arquivo 05_operadores

---
+++ 05_operadores.py
@@ -0,0 +1,32 @@
+nun1 = int(input("informe primeiro numero: \n"))
+nun2 = int(input("informe segundo numero: \n"))
+
+sum = nun1 + nun2
+sub = nun1 - nun2
+multi = nun1 * nun2
+div = nun1 / nun2
+
+print(f"A soma de {nun1} mais {nun2} é: {sum} \n"
+      f"A subtração de {nun1} menos {nun2} é: {sub}\n"
+      f"A multiplicação de {nun1} por {nun2} é: {multi}\n"
+      f"A divisão de {nun1} por {nun2} é: {div} \n"
+      )
+# Comparação
+
+maior = nun1 > nun2
+menor = nun1 < nun2
+igual = nun1 == nun2
+dif = nun1 != nun2
+
+print(f"{nun1} é maior que {nun2}? {maior} \n"
+      f"{nun1} é menor que {nun2}? {menor}\n"
+      f"{nun1} é igual a {nun2}? {igual}\n"
+      f"{nun1} é diferente de {nun2}? {dif} \n"
+      )
+
+# Existe ainda..
+'''
+>=
+<=
+
+'''
